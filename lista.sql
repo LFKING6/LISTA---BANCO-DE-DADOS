cfb062e15ecd472b912b76b1715c5326903d6391
Luis Felipe <luisfelipe.tl2006@gmail.com>
2023-09-11 22:47:20 -0300

    exercicio 3 finalizado

--- lista.sql
+++ lista.sql
@@ -1,3 +1,5 @@
 SELECT titulo AS Titulo_do_Livro FROM livros;
 
 SELECT nome AS Nome_Autor FROM autores WHERE nascimento < '1900-01-01';
+
+SELECT titulo AS Titulo_do_Livro FROM livros WHERE autor_id = 1;
