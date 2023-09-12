e5a941e7594f0db1877758555499a9995ea598da
Luis Felipe <luisfelipe.tl2006@gmail.com>
2023-09-11 22:48:43 -0300

    exercicio 4 finalizado

--- lista.sql
+++ lista.sql
@@ -3,3 +3,8 @@ SELECT titulo AS Titulo_do_Livro FROM livros;
 SELECT nome AS Nome_Autor FROM autores WHERE nascimento < '1900-01-01';
 
 SELECT titulo AS Titulo_do_Livro FROM livros WHERE autor_id = 1;
+
+SELECT alunos.nome AS Nome_Aluno
+FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE curso = 'Engenharia de Software';
